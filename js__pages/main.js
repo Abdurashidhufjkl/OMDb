2b53e01a76e7724ee3cd5edaf4cfb002a579586d
abdurashidhufjkl <abdurashidrashimov@gmail.com>
2026-01-23 17:41:21 +0500

    post recomedeshion

--- js__pages/main.js
+++ js__pages/main.js
@@ -44,9 +44,39 @@ function showData(data) {
 
 function showOneMovie(id) {
   axios.get(`https://www.omdbapi.com/?i=${id}&apikey=d48b840d`).then((res) => {
-    console.log("Movie details:", res.data);
+    const movie = res.data;
+    console.log(res.data);
+
+    elResults.innerHTML = `
+        <div class="movie-detail">
+        <div class="movie-detail-left">
+            <h1 class='movie-detail__title'>${movie.Title}</h1>
+            <img class='movie-detail__img' src="${movie.Poster}" alt="${movie.Title}" />
+        </div>
+
+        <div class="movie-detail-left">
+            <h3><span>ImdbRating: </span>${movie.imdbRating}</h3>
+            <h3><span>ImdbVotes: </span>${movie.imdbVotes}</h3>
+            <h3><span>Metascore:</span> ${movie.Metascore}</h3>
+            <h3><span>Type: </span>${movie.Type}</h3>
+            <h3><span>Released: </span>${movie.Released}</h3>
+            <h3><span>Country: </span>${movie.Country}</h3>
+            <h3><span>Language: </span>${movie.Language}</h3>
+            <h3><span>Genre: </span>${movie.Genre}</h3>
+            <h3><span>BoxOffice: </span>${movie.BoxOffice}</h3>
+            <h3><span>Director: </span>${movie.Director}</h3>
+            <h3><span>Writer: </span>${movie.Writer}</h3>
+            <h3><span>Actors: </span>${movie.Actors}</h3>
+            <h3><span>Awards: </span>${movie.Awards}</h3>
+            <h3><span>Plot: </span>${movie.Plot}</h3>
+            <h3><span>Runtime: </span>${movie.Runtime}</h3>
+            <h3><span>DVD: </span>${movie.DVD}</h3>
+            <h3><span>Website: </span>${movie.Website}</h3>
+            <h3><span>Production: </span>${movie.Production}</h3>
+        </div>
+        </div>
+        `;
   });
 }
-
 
 
